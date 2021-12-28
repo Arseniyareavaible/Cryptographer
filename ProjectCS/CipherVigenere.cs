83a574fe717a3981fc5f3e59a18fbec2df011900
eqfcdarw arseniy <arseny123395@gmail.com>
2021-12-28 12:40:29 +0300

    up

--- ProjectCS/CipherVigenere.cs
+++ ProjectCS/CipherVigenere.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,42 +9,34 @@ namespace ProjectCS
 {
     public class CipherVigenere
     {
-        static Random random = new Random();
-        private static string alphabet = "abcdefghijklmnopqrstuvwxyzабвгдеёжзийклмнопрстуфхцчшщъыьэюя" +
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ" +
-            "0123456789\"!#$%&'()*+,-./:;<=>?[\\]^_`{|}\n ";
-        public static string Encrypt(string text, string key)
-        {
-            string cipherText = string.Empty;
-            for (int i = 0, j = 0; i < text.Length; i++, j++)
-            {
-                if (j >= key.Length)
-                    j = 0;
-                char letter = key[j];
-                int sum = (alphabet.IndexOf(text[i]) + alphabet.IndexOf(letter)) % alphabet.Length;
-                cipherText += alphabet[sum];
-            }
-            return cipherText;
-        }
-        public static string Decrypt(string cipherText, string key)
-        {
-            string line = String.Empty;
-            for (int i = 0, j = 0; i < cipherText.Length; i++, j++)
-            {
-                if (j >= key.Length)
-                    j = 0;
-                char letter = key[j];
-                int sum;
-                if (alphabet.IndexOf(cipherText[i]) < alphabet.IndexOf(letter))
-                {
-                    sum = alphabet.Length + alphabet.IndexOf(cipherText[i]) - alphabet.IndexOf(letter);
-                    line += alphabet[sum];
-                    continue;
-                }
-                sum = alphabet.IndexOf(cipherText[i]) - alphabet.IndexOf(letter);
-                line += alphabet[sum];
-            }
-            return line;
-        }
-    }
+          public static string Encrypt(string encryptedText, string key)
+          {
+               var decryptedText = new StringBuilder();
+
+               for (int i = 0, j = 0; i < encryptedText.Length; i++, j++)
+               {
+                    if (j == key.Length)
+                         j = 0;
+
+                    decryptedText.Append((char)(encryptedText[i] + key[j]));
+               }
+
+               return decryptedText.ToString();
+          }
+
+          public static string Decrypt(string decryptedText, string key)
+          {
+               var encryptedText = new StringBuilder();
+
+               for (int i = 0, j = 0; i < decryptedText.Length; i++, j++)
+               {
+                    if (j == key.Length)
+                         j = 0;
+
+                    encryptedText.Append((char)(decryptedText[i] - key[j]));
+               }
+
+               return encryptedText.ToString();
+          }
+     }
 }
--- ProjectCS/MainWindow.xaml.cs
+++ ProjectCS/MainWindow.xaml.cs
@@ -17,83 +17,113 @@ using Microsoft.Win32;
 
 namespace ProjectCS
 {
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+     public partial class MainWindow : Window
+     {
+          public MainWindow()
+          {
+               InitializeComponent();
+          }
 
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            if(e.LeftButton == MouseButtonState.Pressed)
-            {
-                DragMove();
-            }
-        }
+          private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+          {
+               if (e.LeftButton == MouseButtonState.Pressed)
+               {
+                    DragMove();
+               }
+          }
 
-        private void Button_Save(object sender, RoutedEventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text file (*.txt)|*.txt";
-            saveFileDialog.DefaultExt = ".txt";
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                File.WriteAllText(saveFileDialog.FileName, TextBoxText.Text);
-                TextBoxText.Text = string.Empty;
-                TextBoxKey.Text = string.Empty;
-            }
-        }
+          private void Button_Save(object sender, RoutedEventArgs e)
+          {
+               SaveFileDialog saveFileDialog = new SaveFileDialog();
+               saveFileDialog.Filter = "Text file (*.txt)|*.txt";
+               saveFileDialog.DefaultExt = ".txt";
+               if (saveFileDialog.ShowDialog() == true)
+               {
+                    File.WriteAllText(saveFileDialog.FileName, TextBoxText.Text);
+                    TextBoxText.Text = string.Empty;
+                    TextBoxKey.Text = string.Empty;
+               }
+          }
 
-        private void Button_Import(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog fileDialog = new OpenFileDialog();
-            fileDialog.Filter = "Text file (*.txt)|*.txt";
-            fileDialog.DefaultExt = ".txt";
-            if (fileDialog.ShowDialog() == true)
-            {
-                string path = string.Empty;
-                foreach (string sFilename in fileDialog.FileNames)
-                {
-                    path += sFilename;
-                }
-                string textLines = File.ReadAllText(path);
-                TextBoxText.Text = textLines;
-            }
-        }
+          private void Button_Import(object sender, RoutedEventArgs e)
+          {
+               OpenFileDialog fileDialog = new OpenFileDialog();
+               fileDialog.Filter = "Text file (*.txt)|*.txt";
+               fileDialog.DefaultExt = ".txt";
+               if (fileDialog.ShowDialog() == true)
+               {
+                    string path = string.Empty;
+                    foreach (string sFilename in fileDialog.FileNames)
+                    {
+                         path += sFilename;
+                    }
+                    string textLines = File.ReadAllText(path);
+                    TextBoxText.Text = textLines;
+               }
+          }
 
-        private void Button_Exit(object sender, RoutedEventArgs e)
-        {
-            Environment.Exit(0);
-        }
+          private void Button_Exit(object sender, RoutedEventArgs e)
+          {
+               Environment.Exit(0);
+          }
 
-        private void ButtonEncrypt_Click(object sender, RoutedEventArgs e)
-        {
-            if(TextBoxKey.Text == string.Empty)
-            {
-                MessageBox.Show("Введите ключ!");
-            }
-            else
-            {
-                TextBoxText.Text = CipherVigenere.Encrypt(TextBoxText.Text, TextBoxKey.Text);
-            }
-        }
+          private void ButtonEncrypt_Click(object sender, RoutedEventArgs e)
+          {
+               if (string.IsNullOrEmpty(TextBoxKey.Text))
+               {
+                    TextBoxKey.BorderBrush = new SolidColorBrush(Colors.Red);
+                    MessageBox.Show("Enter key!");
+                    TextBoxKey.BorderBrush = new SolidColorBrush(Colors.White);
+               }
+               else if (string.IsNullOrEmpty(TextBoxText.Text))
+               {
+                    TextBoxText.BorderBrush = new SolidColorBrush(Colors.Red);
+                    MessageBox.Show("Enter text!");
+                    TextBoxText.BorderBrush = new SolidColorBrush(Colors.White);
+               }
+               else
+               {
+                    TextBoxText.Text = CipherVigenere.Encrypt(TextBoxText.Text, TextBoxKey.Text);
+               }
+          }
 
-        private void ButtonDecrypt_Click(object sender, RoutedEventArgs e)
-        {
-            if (TextBoxKey.Text == string.Empty)
-            {
-                MessageBox.Show("Введите ключ!");
-            }
-            else
-            {
-                TextBoxText.Text = CipherVigenere.Decrypt(TextBoxText.Text, TextBoxKey.Text);
-            }
-        }
+          private void ButtonDecrypt_Click(object sender, RoutedEventArgs e)
+          {
+               if (string.IsNullOrEmpty(TextBoxKey.Text))
+               {
+                    TextBoxKey.BorderBrush = new SolidColorBrush(Colors.Red);
+                    MessageBox.Show("Enter key!");
+                    TextBoxKey.BorderBrush = new SolidColorBrush(Colors.White);
+               }
+               else if (string.IsNullOrEmpty(TextBoxText.Text))
+               {
+                    TextBoxText.BorderBrush = new SolidColorBrush(Colors.Red);
+                    MessageBox.Show("Enter text!");
+                    TextBoxText.BorderBrush = new SolidColorBrush(Colors.White);
+               }
+               else
+               {
+                    TextBoxText.Text = CipherVigenere.Decrypt(TextBoxText.Text, TextBoxKey.Text);
+               }
+          }
 
-        private void ButtonQuest_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Программа «Приложение шифровальщик текстов» предназначена для шифрования и дешифрования текстовых файлов по методу Виженера, с помощью функций “Открытие” и “Сохранение” упрощается шифрование больших текстовых файлов.");
-        }
-    }
+          private void ButtonQuest_Click(object sender, RoutedEventArgs e)
+          {
+               MessageBox.Show("Программа «шифратор текстов» предназначена для шифрования и дешифрования текстовых файлов по методу Виженера, с помощью функций “Открытие” и “Сохранение” упрощается шифрование больших текстовых файлов. Доступна функция dragNdrop");
+          }
+
+          private void Window_DragEnter(object sender, DragEventArgs e)
+          {
+               if (e.Data.GetDataPresent(DataFormats.FileDrop))
+               {
+                    e.Effects = DragDropEffects.All;
+               }
+          }
+
+          private void Window_Drop(object sender, DragEventArgs e)
+          {
+               string[] path = (string[])e.Data.GetData(DataFormats.FileDrop);
+               TextBoxText.Text = File.ReadAllText(path[0]);
+          }
+     }
 }
